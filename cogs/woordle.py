d4e93db18680d5f588b4446642ab05b834070122
Jensdboc <jensdb@telenet.be>
2022-12-19 12:35:01 +0100

    Fix multiple channels

--- cogs/woordle.py
+++ cogs/woordle.py
@@ -46,13 +46,16 @@ class Woordle(commands.Cog):
             embed = discord.Embed(title="Woordle", description="Woops, maybe you should start a game in private!", color=ctx.author.color)        
             await ctx.send(embed=embed)
             return
-        channel = self.client.get_channel(878308113604812880)  # Other-games, De Boomhut Van Nonkel Jerry: 878308113604812880 
-                                                               # Bot-spam, De Boomhut Van Nonkel Jerry: 765211470744518658
-                                                               # Private channel: 1003268990476496906
-                                                               # Emilia server: 1039877136179277864
-                                                               # Tom en Jerry: 1054342112474316810
-        channel2 = self.client.get_channel(1039877136179277864)
-        channel3 = self.client.get_channel(1054342112474316810)
+        # channel = self.client.get_channel(878308113604812880)  # Other-games, De Boomhut Van Nonkel Jerry: 878308113604812880 
+        #                                                        # Bot-spam, De Boomhut Van Nonkel Jerry: 765211470744518658
+        #                                                        # Private channel: 1003268990476496906
+        #                                                        # Emilia server: 1039877136179277864
+        #                                                        # Tom en Jerry: 1054342112474316810
+        # channel2 = self.client.get_channel(1039877136179277864)
+        # channel3 = self.client.get_channel(1054342112474316810)
+        
+        channel_ids = [878308113604812880, 1039877136179277864, 1054342112474316810]
+        # channel_ids = [954028408030003292, 954028573830811703, 954028641665286267] # Bot-test
         # Check if there is a current word
         if self.games.word is None:
             embed = discord.Embed(title="Woordle", description="Woops, there is no word yet!", color=ctx.author.color)        
@@ -86,6 +89,7 @@ class Woordle(commands.Cog):
                 WHERE id = ?;
             """, (str(self.counter),))  # This has to be a a string, can't insert integers
             self.db.commit()
+            print("Called")
             return embed
 
         # Get woordle and check if the game is valid
@@ -104,9 +108,13 @@ class Woordle(commands.Cog):
                 self.wordstring += guess
                 woordle_game.message = await ctx.send(embed=embed)
                 if woordle_game.right_guess(guess):
-                    await channel.send(embed = show_results(ctx.author.id))
-                    await channel2.send(embed = show_results(ctx.author.id))
-                    await channel3.send(embed = show_results(ctx.author.id))
+                    result_embed = show_results(ctx.author.id)        
+                    for id in channel_ids:
+                        channel = self.client.get_channel(id)
+                        await channel.send(embed=result_embed)
+                    # await channel.send(embed = show_results(ctx.author.id))
+                    # await channel2.send(embed = show_results(ctx.author.id))
+                    # await channel3.send(embed = show_results(ctx.author.id))
                 woordle_game.add_row()
         elif not woordle_game.playing:
             embed = discord.Embed(title="Woordle", description="You have already finished the Woordle!", color=0xff0000)
@@ -118,20 +126,28 @@ class Woordle(commands.Cog):
             self.wordstring += guess
             await woordle_game.message.edit(embed=embed)
             if woordle_game.right_guess(guess):
-                await channel.send(embed = show_results(ctx.author.id))
-                await channel2.send(embed = show_results(ctx.author.id))
-                await channel3.send(embed = show_results(ctx.author.id))
+                result_embed = show_results(ctx.author.id)        
+                for id in channel_ids:
+                    channel = self.client.get_channel(id)
+                    await channel.send(embed=result_embed)
+                # await channel.send(embed = show_results(ctx.author.id))
+                # await channel2.send(embed = show_results(ctx.author.id))
+                # await channel3.send(embed = show_results(ctx.author.id))
             elif woordle_game.row < 6:
                 woordle_game.add_row()
             else: 
                 woordle_game.stop()
                 timediff = str(timedelta(seconds=(time.time() - woordle_game.timestart)))
                 embed_private = discord.Embed(title="Woordle", description="Better luck next time, the word was " + woordle_game.word + "!", color=ctx.author.color)        
-                embed = discord.Embed(title="Woordle " + str(self.counter) + " "+ "X/6 by " + ctx.author.name + ": " + timediff[:-3], description=woordle_game.display_end(), color=ctx.author.color)        
                 await ctx.send(embed=embed_private)
-                await channel.send(embed=embed)
-                await channel2.send(embed=embed)
-                await channel3.send(embed=embed)
+                for id in channel_ids:
+                    channel = self.client.get_channel(id)
+                    embed = discord.Embed(title="Woordle " + str(self.counter) + " "+ "X/6 by " + ctx.author.name + ": " + timediff[:-3], description=woordle_game.display_end(), color=ctx.author.color)        
+                    # result_embed = show_results(ctx.author.id)    
+                    await channel.send(embed=embed)
+                # await channel.send(embed=embed)
+                # await channel2.send(embed=embed)
+                # await channel3.send(embed=embed)
                 # Process information
                 cur = self.db.cursor()
                 cur.execute('INSERT INTO game VALUES (?, ?, ?, ?, ?, ?)', (id, "failed", timediff, self.counter, self.wordstring, self.wrong_guesses))
