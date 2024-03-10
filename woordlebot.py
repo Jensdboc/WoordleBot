1775ef984c95a0fffce40be50180f0eb3edb43d3
Jensdboc <jensdb@telenet.be>
2024-03-10 16:59:58 +0100

    Update to woordle2.0

--- woordlebot.py
+++ woordlebot.py
@@ -11,7 +11,7 @@ from initialize_database import create_database, fill_database, set_word_of_toda
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix=PREFIX, help_command=CustomHelpCommand(),
                       case_insensitive=True, intents=intents)
-client.activity = discord.Game(name="Join https://discord.gg/wD6TYZFk")
+client.activity = discord.Game(name="Join https://discord.gg/KmN3jh3Fe5")
 
 
 @client.command()
@@ -82,7 +82,7 @@ async def main():
     set_word_of_today()
 
     print("Reading token...")
-    with open('data/token_test.txt', 'r') as file:
+    with open('data/token.txt', 'r') as file:
         token = file.readline()
     await load_extensions()
     await client.start(token)
